49620ad820fa3d4ff6101ff9abb4fecbac564ae8
hkdelrio <46000280+hkdelrio@users.noreply.github.com>
2020-05-16 19:43:11 -0400

    Converted to TypeScript

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,19 @@ import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import { GetStaticProps } from 'next'
 
-export default function Home( { allPostsData}) {
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
-    <Layout home>
+<Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
@@ -37,7 +46,8 @@ export default function Home( { allPostsData}) {
     </Layout>
   )
 }
-export async function getStaticProps() {
+
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
   return {
     props: {
@@ -45,3 +55,4 @@ export async function getStaticProps() {
     }
   }
 }
+
--- pages/posts/testimonies.tsx
+++ pages/posts/testimonies.tsx
@@ -1,11 +1,13 @@
-import Link from 'next/link'
-import Head from 'next/head'
-import Layout from '../../components/layout'
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
     <Layout>
-    <Head><title>God Moments: Testimonies</title></Head>
+      <Head>
+        <title>God Moments: Testimonies</title>
+      </Head>
       <h1>Testimony Stream</h1>
       <h2>
         <Link href="/">
@@ -13,5 +15,5 @@ export default function FirstPost() {
         </Link>
       </h2>
     </Layout>
-  )
+  );
 }
