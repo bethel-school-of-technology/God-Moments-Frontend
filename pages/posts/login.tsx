534032b6c9c1e143ad7338297778a61f6ddfd490
EstherReeves <starlightsak78@gmail.com>
2020-05-28 12:26:59 -0500

    login

--- pages/posts/login.tsx
+++ pages/posts/login.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../../components/layout";
+import utilStyles from '../../styles/utils.module.css'
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 export default function Login() {
@@ -12,9 +13,13 @@ export default function Login() {
     <div style={{
         marginBottom: '5%'
     }}>
-        <Head>
-            <title>God Moments</title>
+        <Head>   
+           <title>God Moments</title>
+          <script>    
+           
             <link rel="icon" href="/favicon.ico"/>
+           
+            </script>
         </Head>
         <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Navbar.Brand href="/">God Moments</Navbar.Brand>
@@ -33,9 +38,50 @@ export default function Login() {
                 </Navbar.Collapse>
         </Navbar>
     </div>
+    
+      <h2>The Kairos Coders welcome you to God Moments</h2>
+      <h4>Log-In Here:</h4>
+      <section className={utilStyles.border}>
+      <form>
+      <section className={utilStyles.form}>
+      <label id="userName">User Name:</label><br/>
+      <input type="text" id="userName" placeholder="Username"/><br/>
+      <label id="passWord">Password:</label><br/>
+      <input type="text" id="passWord" placeholder="Password"/><br/>
+      <br/>
+      <button name="submit" type="submit">Submit!</button>
+      </section>
+      </form>
+      </section>
+      <br/>
+      <br/>
+      <br/>
+      <h4>New User:</h4>
+      <section className={utilStyles.border}>
+        <form >
+      <section className={utilStyles.form}>
+        <label id="firstName">First Name:</label><br/>
+      <input type="text" id="firstName" placeholder="First Name"/><br/>
+      <label id="lastName">Last Name:</label><br/>
+      <input type="text" id="lastName" placeholder="Last Name"/><br/>
+      <label id="birthDate">Birthday: </label><br/>
+      <input type="date" id="birthDate" placeholder="Birthday"/><br/>
+      <label id="email">Email:</label><br/>
+      <input type="email" id="email" placeholder="Email Address"/><br/>
+      <label id="userName">User Name:</label><br/>
+      <input type="text" id="userName" placeholder="User Name"/><br/>
+      <label id="passWord">Password:</label><br/>
+      <input type="text" id="passWord" placeholder="Password"/><br/>
+      <br/>
+      <button name="submit" type="submit">Submit!</button>
+      <br/>
+      </section>
+      
+     </form>
+      </section>
+      
 
-      <h3>The Kairos Coders welcome you to God Moments</h3>
-      <h2>Sign-In Here:</h2>
-    </Layout>
+     
+     </Layout>
   )
-}
+  }
